f0e0dff3c09761f8c03b59468d7875551150bccd
odulaja philip <odulajaphilip@gmail.com>
2022-10-24 23:59:52 +0100

    initialized app

--- useCase_1.py
+++ useCase_1.py
@@ -1,7 +1,8 @@
+from webbrowser import Grail
 import graphene
 from starlette_graphene3 import GraphQLApp, make_graphiql_handler
 from starlette_graphene3.applications import Starlette
-from graphene import ObjectType , String , Int, List 
+from graphene import ObjectType , String , Int, List , Schema 
 
 data = [
     {
@@ -17,3 +18,18 @@ data = [
         "city": "halmiton"
     }
 ]
+
+class Student(ObjectType):
+    name= String()
+    city = String()
+
+class Person(ObjectType):
+    student = List(Student)
+
+    def resolve_student(self, info):
+        return data
+
+
+app = Starlette()
+schema = Schema(query=Person)
+app.mount("/students", GraphQLApp(schema, on_get=make_graphiql_handler()))
