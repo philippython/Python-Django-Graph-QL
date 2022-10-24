8c23b90d539af9f1c7018809fef49f716ff8cce9
odulaja philip <odulajaphilip@gmail.com>
2022-10-24 23:38:23 +0100

    import necessary modules

--- useCase_1.py
+++ useCase_1.py
@@ -1,2 +1,5 @@
 import graphene
-import 
+from starlette_graphene3 import GraphQLApp, make_graphiql_handler
+from starlette_graphene3.applications import Starlette
+from graphene import ObjectType , String , Int, List 
+
