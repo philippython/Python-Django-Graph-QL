b5504737f4983f0746a260253b3fc985ed486919
odulaja philip <odulajaphilip@gmail.com>
2022-10-24 23:43:21 +0100

    added sample data

--- useCase_1.py
+++ useCase_1.py
@@ -3,3 +3,17 @@ from starlette_graphene3 import GraphQLApp, make_graphiql_handler
 from starlette_graphene3.applications import Starlette
 from graphene import ObjectType , String , Int, List 
 
+data = [
+    {
+        "name": "philip",
+        "city": "ontario"
+    },
+    {
+        "name": "john",
+        "city": "ottawa"
+    },
+    {
+        "name": "blessing",
+        "city": "halmiton"
+    }
+]
