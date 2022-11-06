2b12eea1fdcc6c6fc837f3b649dfbbc22b1abdc6
odulaja philip <odulajaphilip@gmail.com>
2022-11-06 16:04:53 +0100

    created query instance

--- Csv_graphQl/metaclass/main.py
+++ Csv_graphQl/metaclass/main.py
@@ -1,3 +1,13 @@
-from graphene import ObjectType
-from starlette.applications import GraphQLApp
-from starlette_graphene3 import Starlette,  make_graphiql_handler
+from graphene import ObjectType, String, Schema
+from starlette_graphene3 import GraphQLApp, make_graphiql_handler
+from starlette.applications import Starlette
+
+class myQuery(ObjectType):
+    hello =  String()
+
+    def resolve_hello(self,info):
+        return "Hello World"
+
+app = Starlette()
+schema = Schema(query=myQuery)
+app.mount("/graphql", GraphQLApp(schema=schema, on_get=make_graphiql_handler()))
