30f1cf8986b98f3cc1e3c22672b12bfb280f80e3
odulaja philip <odulajaphilip@gmail.com>
2022-12-08 23:48:01 +0100

    completed graphene metaclass

--- weather_api/metaclass.py
+++ weather_api/metaclass.py
@@ -4,10 +4,18 @@ from starlette.applications import Starlette
 
 
 class myQuery(ObjectType):
+
+    class Meta:
+        name  = "typeofmetaclass"
+        description = "description from metaclass"
+
     hello=String()
+    bye=String()
 
     def resolve_hello(self, info):
         return "Hello world!!!"
 
+    def resolve_bye(self, info):
+        return "Bye"
 app = Starlette()
 app.mount("/graphql", GraphQLApp(schema=Schema(query=myQuery), on_get=make_graphiql_handler()))
