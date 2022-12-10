ae4d2bc129c54a2a31bbac0e222945cec5270888
odulaja philip <odulajaphilip@gmail.com>
2022-12-10 17:30:39 +0100

    created models for graphql schema

---
+++ graphql_api/models.py
@@ -0,0 +1,10 @@
+from collections import namedtuple
+
+Person = namedtuple("Person", ["email", "age", "first_name", "last_name"])
+
+data = {
+    1 : Person("johndeo@gmail.com", 22, "john", "deo"),
+    2 : Person("jerryblue", 20, "jerry", "blue"),
+    3 : Person("jaradhiggins@gmail.com", 21, "juice", "anthony"),
+    4 :
+}
