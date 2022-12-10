02ffa9f44e5c976ad8ed7732374c3902abcfe6ed
odulaja philip <odulajaphilip@gmail.com>
2022-12-10 20:53:06 +0100

    modified Query class

--- graphql_api/schema.py
+++ graphql_api/schema.py
@@ -1,3 +1,31 @@
-from graphene import ObjectType, String , Int
+from graphene import ObjectType, String , Int, List, Schema
+from models import data
 
-class
+class PersonType(ObjectType):
+    email = String()
+    age = Int()
+    first_name = String()
+    last_name = String()
+
+    def resolve_email(person, info):
+        return person.email
+
+    def resolve_age(person, info):
+        return person.age
+
+    def resolve_first_name(person, info):
+        return person.first_name
+    
+    def resolve_last_name(person, info):
+        return person.last_name
+
+class Query(ObjectType):
+    all_people = List(PersonType)
+
+    def resolve_all_people(root, info):
+        return  data.values()
+
+
+schema = Schema(query=Query)
+
+print(schema.execute("{allPeople{email}}"))
